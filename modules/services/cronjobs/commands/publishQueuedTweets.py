812d8c18c6b2d5372bb75e9a5b9d4cf6efc56b69
ryangerardwilson <ryangerardwilson@gmail.com>
2023-07-20 04:12:28 +0530

    sync

--- modules/services/cronjobs/commands/publishQueuedTweets.py
+++ modules/services/cronjobs/commands/publishQueuedTweets.py
@@ -15,6 +15,8 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 load_dotenv(os.path.join(parent_dir, '.env'))
 
+
+TWEET_SPACING=os.getenv('TWEET_SPACING')
 TWITTER_CONSUMER_KEY=os.getenv('TWITTER_CONSUMER_KEY')
 TWITTER_CONSUMER_SECRET=os.getenv('TWITTER_CONSUMER_SECRET')
 TWITTER_ACCESS_TOKEN=os.getenv('TWITTER_ACCESS_TOKEN')
@@ -60,18 +62,18 @@ def publish_queued_tweets():
 
         payload = {"text": tweet_text}
 
-        # check if any tweet has been posted in the last 72 hours for the same note_id
+        # check if any tweet has been posted in the last TWEET_SPACING  hours for the same note_id
         cursor.execute(
             "SELECT posted_at FROM tweets WHERE note_id = %s AND posted_at > %s ORDER BY posted_at DESC LIMIT 1",
-            (note_id, datetime.datetime.now() - datetime.timedelta(hours=72))
+            (note_id, datetime.datetime.Inow() - datetime.timedelta(hours=TWEET_SPACING))
         )
         last_tweet = cursor.fetchone()
 
-        if last_tweet is not None:  # If a tweet from the same note_id was posted in the last 72 hours
+        if last_tweet is not None:  # If a tweet from the same note_id was posted in the last TWEET_SPACING hours
             # Add the tweet to the 'spaced_tweets' table with a scheduled_at value 72 hours after the last tweet
             cursor.execute(
                 "INSERT INTO spaced_tweets (tweet, scheduled_at, note_id) VALUES (%s, %s, %s)",
-                (tweet_text, last_tweet[0] + datetime.timedelta(hours=72), note_id)
+                (tweet_text, last_tweet[0] + datetime.timedelta(hours=TWEET_SPACING), note_id)
             )
             conn.commit()
 
