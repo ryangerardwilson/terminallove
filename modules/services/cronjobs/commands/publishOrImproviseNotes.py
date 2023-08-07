b73a3d6b61d89cac155dc44947badc721e5be69c
Ryan Gerard Wilson <ryangerardwilson@gmail.com>
2023-08-07 15:49:42 +0530

    sync

--- modules/services/cronjobs/commands/publishOrImproviseNotes.py
+++ modules/services/cronjobs/commands/publishOrImproviseNotes.py
@@ -170,9 +170,7 @@ SENTENCE_START_PROMPTS = [
 
 NOTE_TEXT_IMPROVISATION_PROMPTS = [
     "explain the underlying philosophy of CONCEPT_THEME (in context of the internet) to first year philosohpy students, in less than 300 words, starting with - SENTENCE_START_PROMPTS",
-    "tell me a joke about CONCEPT_THEME",
     "give one example of any ancient philosopher's idea that is analogous to CONCEPT_THEME (in context of the internet), in less than 200 words, starting with - SENTENCE_START_PROMPTS",
-    "give one example of any everyday task a puppy does that is analogous to CONCEPT_THEME (in context of the internet), in less than 200 words, starting with - SENTENCE_START_PROMPTS"
     ]
 
 def publish_or_improvise_notes():
