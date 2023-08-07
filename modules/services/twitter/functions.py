263a84ac572ba29992053014a921626f9707e702
Ryan Gerard Wilson <ryangerardwilson@gmail.com>
2023-08-07 08:20:18 +0530

    sync

--- modules/services/twitter/functions.py
+++ modules/services/twitter/functions.py
@@ -143,4 +143,61 @@ def fn_list_tweets(called_function_arguments_dict):
     print(colored(heading, 'cyan'))
     print(colored(tabulate(df, headers='keys', tablefmt='psql', showindex=False), 'cyan'))
 
+def get_oauth_session():
+
+    oauth = OAuth1Session(
+        TWITTER_CONSUMER_KEY,
+        TWITTER_CONSUMER_SECRET,
+        TWITTER_ACCESS_TOKEN,
+        TWITTER_ACCESS_TOKEN_SECRET,
+    )
+
+    response = oauth.post("https://api.twitter.com/2/tweets")
+    if response.status_code != 201:
+        # Load existing tokens from file, if available
+        tokens_file = f"{parent_dir}/files/tokens/{TWITTER_AUTHENTICATION_KEY}"
+        try:
+            with open(tokens_file, 'r') as f:
+                tokens = json.load(f)
+            access_token = tokens['access_token']
+            access_token_secret = tokens['access_token_secret']
+        except FileNotFoundError:
+            # Get new tokens and save them to file
+            request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+            oauth = OAuth1Session(TWITTER_CONSUMER_KEY, client_secret=TWITTER_CONSUMER_SECRET)
+            try:
+                fetch_response = oauth.fetch_request_token(request_token_url)
+            except ValueError:
+                print("There may have been an issue with the consumer_key or consumer_secret you entered.")
+            resource_owner_key = fetch_response.get("oauth_token")
+            resource_owner_secret = fetch_response.get("oauth_token_secret")
+            print("Got OAuth token: %s" % resource_owner_key)
+            base_authorization_url = "https://api.twitter.com/oauth/authorize"
+            authorization_url = oauth.authorization_url(base_authorization_url)
+            print("Please go here and authorize: %s" % authorization_url)
+            verifier = input("Paste the PIN here: ")
+            access_token_url = "https://api.twitter.com/oauth/access_token"
+            oauth = OAuth1Session(
+                TWITTER_CONSUMER_KEY,
+                client_secret=TWITTER_CONSUMER_SECRET,
+                resource_owner_key=resource_owner_key,
+                resource_owner_secret=resource_owner_secret,
+                verifier=verifier,
+            )
+            oauth_tokens = oauth.fetch_access_token(access_token_url)
+            access_token = oauth_tokens["oauth_token"]
+            access_token_secret = oauth_tokens["oauth_token_secret"]
+            # Save tokens to file
+            with open(tokens_file, 'w') as f:
+                json.dump({'access_token': access_token, 'access_token_secret': access_token_secret}, f)
+        # Use the tokens for the API call
+        oauth = OAuth1Session(
+            TWITTER_CONSUMER_KEY,
+            client_secret=TWITTER_CONSUMER_SECRET,
+            resource_owner_key=access_token,
+            resource_owner_secret=access_token_secret,
+        )
+
+    return oauth
+
 
